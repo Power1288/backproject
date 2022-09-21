74857cfc86ad92b3493918b86e1a650f1e0d7ef0
JLPower <63203677+Power1288@users.noreply.github.com>
2022-09-21 12:38:40 +0200

    create userSchema

---
+++ dist/models/user.model.js
@@ -0,0 +1,55 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EUserRole = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
+var EUserRole;
+(function (EUserRole) {
+    EUserRole["user"] = "user";
+    EUserRole["admin"] = "admin";
+})(EUserRole = exports.EUserRole || (exports.EUserRole = {}));
+const userSchema = new mongoose_1.default.Schema({
+    email: {
+        type: String,
+        nullable: false,
+        unique: true
+    },
+    password: {
+        type: String,
+        nullable: false
+    },
+    pseudo: {
+        type: String,
+        nullable: false,
+    },
+    role: {
+        type: String,
+        enum: EUserRole,
+        default: EUserRole.user
+    }
+});
+userSchema.pre("save", function (next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const passwordHash = yield bcrypt_1.default.hash(this.password, 10);
+        this.password = passwordHash;
+        next();
+    });
+});
+userSchema.methods.verifyPassword = function (password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const same = yield bcrypt_1.default.compare(password, this.password);
+        return same;
+    });
+};
---
+++ models/user.model.ts
@@ -0,0 +1,48 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt"
+
+export enum EUserRole {
+    user = "user",
+    admin = "admin"
+}
+
+interface IUser {
+    email:string,
+    password:string,
+    pseudo:string,
+    role:EUserRole,
+    verifyPassword : (password: string) => boolean
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema<IUser>({
+    email: {
+        type:String,
+        nullable:false,
+        unique:true
+    },
+    password: {
+        type:String,
+        nullable:false
+    },
+    pseudo: {
+        type: String,
+        nullable:false,
+    },
+    role: {
+        type:String,
+        enum:EUserRole,
+        default:EUserRole.user
+    }
+})
+
+userSchema.pre("save",async function (next) {
+    const passwordHash = await bcrypt.hash(this.password,10)
+    this.password = passwordHash
+    next()
+})
+
+userSchema.methods.verifyPassword = async function (password: string) : Promise<boolean> {
+    const same : boolean = await bcrypt.compare(password,this.password)
+    return same
+}
+
